6fd07560a84d400bc04eda31e2b6308c7b882d38
DevdbShin <runcoededb.gmail.com>
2023-10-10 18:38:46 +0900

    memoryscore

---
+++ src/level/one/memoryscore/MemoryScore.java
@@ -0,0 +1,16 @@
+package level.one.memoryscore;
+
+public class MemoryScore {
+    public static void main(String[] args) {
+        MemoryScore ms = new MemoryScore();
+        ms.solution(new String [] {"may", "kein", "kain", "radi"}
+                    ,new int [] {5, 10, 1, 3}
+                    ,new String[][] { {"may", "kein", "kain", "radi"}
+                        ,{"may", "kein", "brin", "deny"}
+                        ,{"kon", "kain", "may", "coni"} });
+    }
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = {};
+        return answer;
+    }
+}
