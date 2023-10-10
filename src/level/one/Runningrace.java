ff64ce53bf822339b5fe93dc2f8e43d218b276a8
DevdbShin <runcoededb.gmail.com>
2023-10-10 12:48:54 +0900

    Initial commit

---
+++ src/level/one/Runningrace.java
@@ -0,0 +1,13 @@
+package level.one;
+
+public class Runningrace {
+    public static void main(String[] args) {
+        String [] str = solution(null, null);
+        System.out.println(str.length);
+    }
+
+    public static String[] solution(String[] players, String[] callings) {
+        String[] answer = {};
+        return answer;
+    }
+}
